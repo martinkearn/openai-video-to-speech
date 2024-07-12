655acde219c33e75c328d20c92b9b2442610de22
Martin Kearn <martinkearn@live.co.uk>
2024-07-12 14:52:48 +0100

    Working with audio and transcription

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # openai-video-to-speech
 A Python script which converts an MP4 video to speech using Azure OpenAI services.
+
+The script does the following:
+1. Take an MP4 video as a input
+1. Extracts the audio track as an MP3
+
+## Get Started
+1. Add a file called `video.mp4` to the repository root
+1. Create a Python Virtual Environment `python3 -m venv venv`
+1. Activate virtual environment using `venv\Scripts\activate` for Windows or `source venv/bin/activate` on MacOS/Linux
+1. Install dependencies `pip3 install -r requirements.txt`
+1. Run script `python main.py`
--- main.py
+++ main.py
@@ -1,10 +1,31 @@
+import os
+from openai import AzureOpenAI
+from dotenv import load_dotenv
 from moviepy.editor import VideoFileClip
 
-# Load the video file
+# Load the .env file
+load_dotenv()
+
+# Set vars
+audio_file_name="output_audio.wav"
+
+# Get the audio from the video
+print("Getting the audio from the video")
 video = VideoFileClip("video.mp4")
-
-# Extract the audio
 audio = video.audio
+audio.write_audiofile(audio_file_name)
 
-# Write the audio to a file
-audio.write_audiofile("output_audio.mp3")
+# Get the audio as a transcript via OpenAI
+print("Transcribing the audio")
+transcription_client = AzureOpenAI(
+    api_key = os.getenv("AZURE_OPENAI_API_KEY"),  
+    api_version = "2024-02-15-preview",
+    azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+)
+
+transcription_result = transcription_client.audio.transcriptions.create(
+    file=open("./"+audio_file_name, "rb"),            
+    model=os.getenv("AZURE_OPENAI_TRANSCRIPTION_DEPLOYMENT")
+)
+
+print(transcription_result)
