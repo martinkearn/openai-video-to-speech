51c2d3368304f5ab7aa9d789df54b76ff741515d
Martin Kearn <martinkearn@live.co.uk>
2024-07-17 13:49:37 +0100

    Support Azure Speech Service

--- README.md
+++ README.md
@@ -6,11 +6,23 @@ The script does the following:
 1. Creates a transcription from the video's audio
 1. Create an OpenAI text-to-speech version of the transcription
 
-## Setup OpenAI Services
+## Setup OpenAI Service
+If you want to use Azure OpenAI voices, you need to setup an Azure OpenAI service.
+
 1. Create an Azure OpenAI service in the North Central US or Sweden Central regions using the steps outlined at https://learn.microsoft.com/en-gb/azure/ai-services/openai/whisper-quickstart?tabs=command-line%2Cpython-new&pivots=programming-language-python 
 1. Obtain the `Endpoint` and `Key` from [Azure Portal](https://portal.azure.com/) > The OpenAI service you just created > Keys and Endpoints
 1. Using the [Azure OpenAI Studio](https://oai.azure.com/) > Deployments, deploy a `tts-hd` model using the steps outlined in https://learn.microsoft.com/en-gb/azure/ai-services/openai/how-to/create-resource?pivots=web-portal#deploy-a-model take a note of the deployment name
 1. Using the [Azure OpenAI Studio](https://oai.azure.com/) > Deployments, deploy a `Whisper` model using the steps outlined in https://learn.microsoft.com/en-gb/azure/ai-services/openai/how-to/create-resource?pivots=web-portal#deploy-a-model take a note of the deployment name
+1. Select a voice to use from https://platform.openai.com/docs/guides/text-to-speech. Use `alloy` if unsure
+
+## Setup Azure Speech Service
+If you want to use Azure Speech Service voices, you need to setup an Azure Speech service. These voices are not as natural as OpenAI but there is a much broader choice of languages, accents and controls.
+
+> You do can do the same thing that this code is doing with the Azure Speech Service through the [Azure Speech Studio](https://speech.microsoft.com/portal)
+
+1. Open the [Azure Speech Studio](https://speech.microsoft.com/portal)
+1. Go to the Voice Gallery
+1. Create a `Speech resource` and note the `region` and `resource key`
 
 ## Setup Python Environment
 1. Create an `.env` file which contains these env variables. Replace the real values with values from the steps above
@@ -18,8 +30,10 @@ The script does the following:
 AZURE_OPENAI_API_KEY=Key
 AZURE_OPENAI_ENDPOINT=Endpoint
 AZURE_OPENAI_TTS_DEPLOYMENT=TtsDeploymentName
-AZURE_OPENAI_TTS_VOICE=alloy (or choose any of the OpenAI voice options see https://platform.openai.com/docs/guides/text-to-speech)
 AZURE_OPENAI_WHISPER_DEPLOYMENT=WhisperDeploymentname
+AZURE_SPEECH_KEY=Key
+AZURE_SPEECH_REGION=uksouth
+TTS_VOICE=alloy
 ```
 1. Create a Python Virtual Environment `python3 -m venv venv`
 1. Activate virtual environment using `venv\Scripts\activate` for Windows or `source venv/bin/activate` on MacOS/Linux
@@ -33,6 +47,6 @@ AZURE_OPENAI_WHISPER_DEPLOYMENT=WhisperDeploymentname
 1. You can optionally edit this transcription before converting it to AI speech
 
 ## Create AI speech audio from transcription
-1. Run script `python tts.py`
-1. This will generate a file called `output_tts_audio.mp3` which is the text-to-speech AI audio based on the contents of `output_transcription.txt`
+1. Run script `python tts.py`. You can optionally pass in a voice as a parameter, which can be one of the 6 OpenAI voices (`alloy`, `echo`, `fable`, `onyx`, `nova`, `shimmer`) or one of the many Azure Speech Service voices such as `en-GB-BellaNeural`. For example `python tts.py alloy` or `python tts.py en-GB-BellaNeural`. If nothing is passed, the script defaults to what is in the `.env` file under `TTS_VOICE` which is OpenAI Alloy.
+1. This will generate a file called `output_tts_audio_VOICE.mp3` which is the text-to-speech AI audio based on the contents of `output_transcription.txt`
 
--- tts.py
+++ tts.py
@@ -2,6 +2,7 @@ import os
 import requests
 import json
 import sys
+import azure.cognitiveservices.speech as speechsdk
 from dotenv import load_dotenv
 
 # Load the .env file
@@ -11,35 +12,94 @@ load_dotenv()
 if len(sys.argv) > 1:
     voice = sys.argv[1]
 else:
-    voice = os.getenv("AZURE_OPENAI_TTS_VOICE")
+    voice = os.getenv("TTS_VOICE")
 
 # Set vars
 transcription_file_name="output_transcription.txt"
 audio_tts_file_name="output_tts_audio_" +voice +".mp3"
 
-# Get the speech audio from transcription
-print("Converting " +transcription_file_name +" to AI text-to-speech audio with the " +voice +" voice")
+# Set OpenAI voice dicyionary. Avaliable voices from https://platform.openai.com/docs/guides/text-to-speech/quickstart
+open_ai_voices = {
+    "alloy": 1,
+    "echo": 2,
+    "fable": 3,
+    "onyx": 4,
+    "nova": 5,
+    "shimmer": 6
+}
+
+# Get the transcription
 with open(transcription_file_name, "r") as file:
     # Read the entire content of the file
     transcription_from_file = file.read()
-tts_url = os.getenv("AZURE_OPENAI_ENDPOINT") +"/openai/deployments/" +os.getenv("AZURE_OPENAI_TTS_DEPLOYMENT") +"/audio/speech?api-version=2024-02-15-preview"
-tts_headers_list = {
- "api-key": os.getenv("AZURE_OPENAI_API_KEY"),
- "Content-Type": "application/json" 
-}
-tts_payload = json.dumps({
-    "model": "tts-1-hd",
-    "input": str(transcription_from_file),
-    "voice": voice
-})
-tts_response = requests.request("POST", tts_url, data=tts_payload,  headers=tts_headers_list)
-print()
 
-if tts_response.status_code == 200:
-    # Save the binary content to a file
-    with open(audio_tts_file_name, 'wb') as file:
-        file.write(tts_response.content)
-    print("TTS output file saved successfully as " +audio_tts_file_name)
+# Process OpenAI Voice
+def openai():
+    print("Converting " +transcription_file_name +" with the Azure OpenAI and the " +voice +" voice")
+
+    # Make API call
+    tts_url = os.getenv("AZURE_OPENAI_ENDPOINT") +"/openai/deployments/" +os.getenv("AZURE_OPENAI_TTS_DEPLOYMENT") +"/audio/speech?api-version=2024-02-15-preview"
+    tts_headers_list = {
+    "api-key": os.getenv("AZURE_OPENAI_API_KEY"),
+    "Content-Type": "application/json" 
+    }
+    tts_payload = json.dumps({
+        "model": "tts-1-hd",
+        "input": str(transcription_from_file),
+        "voice": voice
+    })
+    tts_response = requests.request("POST", tts_url, data=tts_payload,  headers=tts_headers_list)
+    print()
+
+    if tts_response.status_code == 200:
+        # Save the binary content to a file
+        with open(audio_tts_file_name, 'wb') as file:
+            file.write(tts_response.content)
+        print("TTS output file saved successfully as " +audio_tts_file_name)
+    else:
+        print(f"Failed to retrieve content. Status code: {tts_response.status_code}")
+        print(f"Response content: {tts_response.content}")
+
+
+# Process Azure Speech Service Voice
+def azurespeech():
+    print("Converting " +transcription_file_name +" with Azure Speech Service and the " +voice +" voice")
+
+    # Creates an instance of a speech config with specified subscription key and service region.
+    speech_service_key = os.getenv("AZURE_SPEECH_KEY")
+    speech_service_region = os.getenv("AZURE_SPEECH_REGION")
+    speech_config = speechsdk.SpeechConfig(subscription=speech_service_key, region=speech_service_region)
+    speech_config.speech_synthesis_voice_name = voice
+    audio_output = speechsdk.audio.AudioOutputConfig(filename=audio_tts_file_name)
+    synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_output)
+
+    # Perform the speech synthesis
+    result = synthesizer.speak_text_async(str(transcription_from_file)).get()
+
+    # Check result status
+    if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+        print("TTS output file saved successfully as " +audio_tts_file_name)
+    elif result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = result.cancellation_details
+        print(f"Speech synthesis canceled: {cancellation_details.reason}")
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            print(f"Error details: {cancellation_details.error_details}")
+
+# If the voice is an OpenAI voice, use that service, otherwise process as a Azure Speech voice
+if voice in open_ai_voices:
+    openai()
 else:
-    print(f"Failed to retrieve content. Status code: {tts_response.status_code}")
-    print(f"Response content: {tts_response.content}")
+    azurespeech()
+
+
+
+
+
+
+
+
+
+
+
+
+
