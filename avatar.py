9cdf8b96eff63aa1fd3b1e4ae94492352f452b6d
Martin Kearn <martinkearn@live.co.uk>
2024-07-26 09:02:42 +0100

    Using env vars better

--- avatar.py
+++ avatar.py
@@ -10,7 +10,6 @@ import os
 import sys
 import time
 import uuid
-  
 import requests
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,  # set to logging.DEBUG for verbose output
@@ -18,12 +17,13 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO,  # set to logging.DEB
 logger = logging.getLogger(__name__)
 
 
-# The endpoint (and key) could be gotten from the Keys and Endpoint page in the Speech service resource.
-# The endpoint would be like: https://<region>.api.cognitive.microsoft.com or https://<custom_domain>.cognitiveservices.azure.com
-# If you want to use passwordless authentication, custom domain is required.
-SPEECH_ENDPOINT = os.getenv('SPEECH_ENDPOINT',"https://northeurope.api.cognitive.microsoft.com")
-# We recommend to use passwordless authentication with Azure Identity here; meanwhile, you can also use a subscription key instead
+SPEECH_ENDPOINT = os.getenv('SPEECH_ENDPOINT',"https://swedencentral.api.cognitive.microsoft.com/")
 API_VERSION = "2024-04-15-preview"
+AVATAR_VIDEO_OUTPUT_FILENAME = "output/output_avatar.mp4"
+AVATAR_CHARACTER = "max" # talking avatar character
+AVATAR_STYLE = "business",  # talking avatar style, required for prebuilt avatar, optional for custom avatar
+AVATAR_VOICE = "en-US-OnyxMultilingualNeural"
+INPUT_TEXT = "So what is a workspace label? When we upload TDF files to C, we need a way to route the TDF file to the correct workspace. Traditionally, we'd use something like a UUID for this which is a long alphanumeric string. The problem with this approach is that the high side (C) is not connected to the high side on premise. This makes it difficult for humans to remember and verbally share the ID. "
 
 
 def _create_job_id():
@@ -32,7 +32,7 @@ def _create_job_id():
     return uuid.uuid4()
 
 def _authenticate():
-    SUBSCRIPTION_KEY = os.getenv("SUBSCRIPTION_KEY", '7f5b6098b9744f6d9f2c9da08d8bd343')
+    SUBSCRIPTION_KEY = os.getenv("AZURE_SPEECH_KEY")
     return {'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY}
 
 
@@ -45,21 +45,17 @@ def submit_synthesis(job_id: str):
 
     payload = {
         'synthesisConfig': {
-            "voice": 'en-GB-RyanNeural',
+            "voice": "{AVATAR_VOICE}",
         },
         "inputKind": "plainText",
         "inputs": [
             {
-                "content": "So what is a workspace label? When we upload TDF files to C, we need a way to route the TDF file to the correct workspace. Traditionally, we'd use something like a UUID for this which is a long alphanumeric string. The problem with this approach is that the high side (C) is not connected to the high side on premise. This makes it difficult for humans to remember and verbally share the ID. ",
+                "content": INPUT_TEXT
             },
         ],
         "avatarConfig": {
-            "talkingAvatarCharacter": "max",  # talking avatar character
-            "talkingAvatarStyle": "business",  # talking avatar style, required for prebuilt avatar, optional for custom avatar
-            "videoFormat": "webm",  # mp4 or webm, webm is required for transparent background
-            "videoCodec": "vp9",  # hevc, h264 or vp9, vp9 is required for transparent background; default is hevc
-            "subtitleType": "soft_embedded",
-            "backgroundColor": "transparent"
+            "talkingAvatarCharacter": "{AVATAR_CHARACTER}",  
+            "talkingAvatarStyle": "{AVATAR_STYLE}"
         }
     }
 
@@ -82,22 +78,23 @@ def get_synthesis(job_id):
         logger.debug(response.json())
         if response.json()['status'] == 'Succeeded':
             logger.info(f'Batch synthesis job succeeded, download URL: {response.json()["outputs"]["result"]}')
-        return response.json()['status']
+
+            # Download the video
+            download_response = requests.get(response.json()["outputs"]["result"], stream=True)
+            if download_response.status_code == 200:
+                # Open a local file with write-binary mode
+                with open(AVATAR_VIDEO_OUTPUT_FILENAME, 'wb') as file:
+                    # Iterate over the response data in chunks
+                    for chunk in download_response.iter_content(chunk_size=8192):
+                        # Write each chunk to the file
+                        file.write(chunk)
+                print(f"File downloaded successfully: {AVATAR_VIDEO_OUTPUT_FILENAME}")
+            else:
+                print(f"Failed to download file. Status code: {response.status_code}")
+            
+            return response.json()['status']
     else:
         logger.error(f'Failed to get batch synthesis job: {response.text}')
-
-
-def list_synthesis_jobs(skip: int = 0, max_page_size: int = 100):
-    """List all batch synthesis jobs in the subscription"""
-    url = f'{SPEECH_ENDPOINT}/avatar/batchsyntheses?api-version={API_VERSION}&skip={skip}&maxpagesize={max_page_size}'
-    header = _authenticate()
-
-    response = requests.get(url, headers=header)
-    if response.status_code < 400:
-        logger.info(f'List batch synthesis jobs successfully, got {len(response.json()["values"])} jobs')
-        logger.info(response.json())
-    else:
-        logger.error(f'Failed to list batch synthesis jobs: {response.text}')
 
 
 if __name__ == '__main__':
