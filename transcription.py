9bf8c9470dc9c78f205258d28225501de4269f1d
Martin Kearn <martinkearn@live.co.uk>
2024-08-07 13:38:40 +0100

    Using argparse

--- transcription.py
+++ transcription.py
@@ -1,31 +1,43 @@
 import os
 import sys
+import argparse
 from openai import AzureOpenAI
 from dotenv import load_dotenv
 from moviepy.editor import VideoFileClip
 
+parser = argparse.ArgumentParser(
+    description='transcription.py - Converts a video into an MP3 audio track and then produces TXT transcription based on the audio track. Output files will be in /output/transcription.'
+)
+
+parser.add_argument(
+    '-v', '--videofilename',
+    type=str, 
+    help='The path and file name of the input video. Example input/video.mov'
+)
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Show help message if no arguments are provided
+if len(sys.argv)==1:
+    parser.print_help(sys.stderr)
+    sys.exit(1)
+
 # Load the .env file
 load_dotenv()
-
-# Get args if they exist
-if len(sys.argv) > 1:
-    video_file_name = sys.argv[1]
-else:
-    video_file_name = os.getenv("input/video.mov")
 
 # Set vars
 audio_file_name="output/transcription/output_original_audio.mp3"
 transcription_file_name="output/transcription/output_transcription.txt"
 
 # Get the audio from the video
-print("Getting the audio from the video " +video_file_name)
-video = VideoFileClip(video_file_name)
+print("1. Getting the audio from the video " +args.videofilename)
+video = VideoFileClip(args.videofilename)
 audio = video.audio
 audio.write_audiofile(audio_file_name)
-print()
 
 # Get the audio as a transcript via OpenAI
-print("Transcribing the audio")
+print("2. Transcribing the audio")
 transcription_client = AzureOpenAI(
     api_key = os.getenv("AZURE_OPENAI_API_KEY"),  
     api_version = "2024-02-15-preview",
@@ -38,9 +50,6 @@ transcription_result = transcription_client.audio.transcriptions.create(
 
 with open(transcription_file_name, "w") as file:
     file.write(transcription_result.text)
-print()
 
-print("We have a transcription result (has also been written to " +transcription_file_name +")")
-print()
-
-print(transcription_result.text)
+print("3. Written transcription to " +transcription_file_name)
+print("Done")
